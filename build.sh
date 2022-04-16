2de60455a6af4fd42a49922ad9594092e3b15519
ysadler <103801191+ysadler@users.noreply.github.com>
2022-04-15 22:53:07 -0400

    Update build.sh
    Broken download link for libtool. Changed for https://ftp.gnu.org/libtool

--- build.sh
+++ build.sh
@@ -949,7 +949,7 @@ ln -sf ${PYTHON_CONFIG} ${INSTALL_DIR}/usr/bin/python-config
 
 (
   P=libtool-2.4.6
-  URL="http://gnu.spinellicreations.com/libtool/${P}.tar.xz"
+  URL="https://ftp.gnu.org/gnu/libtool/${P}.tar.xz"
   CKSUM=sha256:7c87a8c2c8c0fc9cd5019e402bed4292462d00a718a7cd5f11218153bf28b26f
 
   SKIP_AUTORECONF=yes
